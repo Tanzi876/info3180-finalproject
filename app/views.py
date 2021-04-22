8431e88618218acd4fec6ccf6329882c34d2016f
Tanzi876 <tanyaleelawrence@gmail.com>
2021-04-22 13:51:56 -0500

    JWT Authenication Added

--- app/views.py
+++ app/views.py
@@ -4,8 +4,9 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-from logging import error
+# from logging import error
 import os,time, base64, jwt
+from functools import wraps
 from app import app,db,csrf
 #  login_manager
 from flask import render_template, request, redirect, url_for, flash, jsonify
@@ -15,6 +16,40 @@ from app.models import Users,Favourites,Cars
 from werkzeug.utils import secure_filename
 from werkzeug.security import check_password_hash
 from flask.helpers import send_from_directory
+
+#Decorator functions for JWT Authentication
+def requires_auth(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    auth = request.headers.get('Authorization', None)
+    if not auth:
+      return jsonify({'code': 'authorization_header_missing', 'description': 'Authorization header is expected'}), 401
+
+    parts = auth.split()
+
+    if parts[0].lower() != 'bearer':
+      return jsonify({'code': 'invalid_header', 'description': 'Authorization header must start with Bearer'}), 401
+    elif len(parts) == 1:
+      return jsonify({'code': 'invalid_header', 'description': 'Token not found'}), 401
+    elif len(parts) > 2:
+      return jsonify({'code': 'invalid_header', 'description': 'Authorization header must be Bearer + \s + token'}), 401
+
+    token = parts[1]
+    try:
+         payload = jwt.decode(token, app.config['SECRET_KEY'])
+
+    except jwt.ExpiredSignature:
+        return jsonify({'code': 'token_expired', 'description': 'token is expired'}), 401
+    except jwt.DecodeError:
+        return jsonify({'code': 'token_invalid_signature', 'description': 'Token signature is invalid'}), 401
+
+    current_user = user = payload
+    return f(*args, **kwargs)
+
+  return decorated
+
+
+
 
 ###
 # Routing for your application.
