e2e10f3ddc786dffa8f509aeb53a939559d15054
Tanzi876 <tanyaleelawrence@gmail.com>
2021-04-23 17:36:50 -0500

    Fixing Error

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -6,8 +6,17 @@ const Explore={
     <div class ="row">
       <form id="search-form" @submit.prevent='search' enctype='multipart/form-data' novalidate>
           <div class="input-group">
-          </div
+          <div class="form-group">
+              <label for="car_make">Make</label>
+              <input type="text" class="form-control" id="car_make" name="car_make">
+          </div>
+           <div class="form-group">
+               <label for="car_model">Model</label>
+               <input type="text" class="form-control" id="car_model" name="car_model">
+           </div>
+          </div>
       </form>
+      <button class="btn btn-primary">Search</button>
     </div>
       `,
       methods:{
@@ -51,98 +60,42 @@ const Explore={
       }
 }
 
-// const Login = Vue.component('login', {
-//     template:`
-//       <div>
-//         <form id="login-form" @submit.prevent="login">
-//             <div class="card-header center">
-//               <strong>Login to your account</strong>
-//             </div>
-//             <div class="card center">
-//               <div class="card-body login">
-//                 <div style="margin-top:5%;">
-//                   <label for='username'><strong>Username</strong></label><br>
-//                   <input type='text' id='username' name='username' style="width: 100%;"/>
-//                 </div>
-//                 <div style="margin-top:5%;">
-//                   <label for='password'><strong>Password</strong></label><br>
-//                   <input type='password' id='password' name='password' style="width: 100%;"/>
-//                 </div>
-//                 <div style="margin-top:5%;">
-//                   <button id="submit" class="btn btn-success">Login</button> 
-//                 </div>
-//                 <div v-if='messageFlag' style="margin-top:5%;">
-//                   <div class="alert alert-danger center" style="width: 100%; margin-top: 5%;">
-//                     {{ message }}
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//         </form>
-//       </div>
-//     `,
-//     methods:{
-//       login: function(){
-//         const self = this
+
+
+const Logout = {
+    name:'logout',
+    template: `
+    <div>
+    <div/>`,
+    created: function () {
+      let self = this;
       
-//         let login_data = document.getElementById('login-form');
-//         let login_form = new FormData(login_data);
-      
-//         fetch("/api/auth/login",{
-//           method: "POST",
-//           body: login_form,
-//           headers: {
-//           'X-CSRFToken': token
-//           },
-//           credentials: 'same-origin'
-//         }).then(function(response){
-//           return response.json();
-//         }).then(function(jsonResponse){
-//           self.messageFlag = true;
-        
-//           if(jsonResponse.hasOwnProperty("token")){
-//             cuser={"token":jsonResponse.token, id: jsonResponse.user_id};
-//             localStorage.current_user = JSON.stringify(cuser);
+      fetch('/api/auth/logout', {
+          method: 'GET',
+          headers: {
+              'X-CSRFToken': token,
+              'Authorization': 'Bearer ' + localStorage.getItem('token')
+          },
+          credentials: 'same-origin'
+      })
+      .then(resp => resp.json())
+      .then(function(jsonResp) {
           
-//             router.go();
-//             router.push("/explore")
-//           }else{
-//             self.message = jsonResponse.errors
-//           }
-
-//         }).catch(function(error){
-//           self.messageFlag = false;
-//           console.log(error);
-//         });
-//       }
-//     },
-//     data: function(){
-//       return {
-//         messageFlag: false,
-//         message: ""
-//       }
-//     }
-// });
-
-// const Logout = Vue.component("logout", {
-//     template: `
-//     <div>
-//     <div/>`,
-//     created: function(){
-//         const self = this;
-//         fetch("api/auth/logout", {
-//         method: "GET"
-//     }).then(function(response){
-//       return response.json();
-//     }).then(function(jsonResponse){
-//       localStorage.removeItem("current_user");
-//       router.go();
-//       router.push("/");
-//     }).catch(function(error){
-//       console.log(error);
-//     });
-//   }
-// });
+          self.message = jsonResp.message;
+          self.error = jsonResp.error;
+          
+          if (self.message) {
+              localStorage.removeItem('token');
+              router.push({name: 'home', params: {response: self.message}})
+          } else {
+              router.push({name: 'home'})
+          }
+      })
+      .catch(function(error) {
+          console.log(error)
+      })
+  },
+};
 
 const Login = {
   name:'login',
@@ -194,7 +147,7 @@ const Login = {
           self.messageFlag = true;
         
           if(jsonResponse.hasOwnProperty("token")){
-            cuser={"token":jsonResponse.token, id: jsonResponse.user_id};
+           let cuser={"token":jsonResponse.token, id: jsonResponse.user_id};
             localStorage.current_user = JSON.stringify(cuser);
           
             router.go();
@@ -337,6 +290,18 @@ const Home ={
       }
     };
 
+    const NotFound = {
+      name: 'NotFound',
+      template: `
+      <div>
+          <h1>404 - Not Found</h1>
+      </div>
+      `,
+      data() {
+          return {}
+      }
+  };
+
 
 
 const app = Vue.createApp({
@@ -348,7 +313,10 @@ const app = Vue.createApp({
   component:{
     'home':Home,
     'register':Register, 
-    'explore':Explore 
+    'explore':Explore,
+    'login': Login,
+    'logout': Logout
+
   }
 });
 
@@ -358,7 +326,10 @@ const router=VueRouter.createRouter({
     {path:'/',component:Home},
     {path:'/register',component:Register},
     {path:'/login',component:Login},
-    {path:'/explore',component:Explore}
+    {path: "/logout", name: "logout", component: Logout},
+    {path:'/explore',component:Explore},
+    {path: "*", component: NotFound}
+
   ]
 })
 
@@ -382,29 +353,29 @@ app.component('app-header', {
           </nav>
       </header>    
   `,
-//   watch: {
-//     '$route' (to, fom){
-//         this.reload()
-//     }
-//   },
-// created() {
-//     let self = this;
-//     self.user=localStorage.getItem('token');
-//     self.userid=localStorage.getItem('userid')
-// },
-//   data() {
-//     return {
-//       //User array
-//     };
-//   },
-//   methods:{
-//     reload(){
-//         let self = this;
-//         self.user=localStorage.getItem('token');
-//         self.userid=localStorage.getItem('userid')
-//     }
-// }
-// });
+  watch: {
+    '$route' (to, fom){
+        this.reload()
+    }
+  },
+created() {
+    let self = this;
+    self.user=localStorage.getItem('token');
+    self.userid=localStorage.getItem('userid')
+},
+  data() {
+    return {
+      //User array
+    };
+  },
+  methods:{
+    reload(){
+        let self = this;
+        self.user=localStorage.getItem('token');
+        self.userid=localStorage.getItem('userid')
+    }
+}
+});
 
 app.component('app-footer', {
   name: 'AppFooter',
