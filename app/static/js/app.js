8be59ad57acbc2d5803ee119b1b5fd4621a84616
Tanzi876 <tanyaleelawrence@gmail.com>
2021-04-26 21:48:50 -0500

    Search Function Added

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -46,10 +46,10 @@ const Explore={
       computed:{
         filteredpost(){
           if (car_make != ''){
-            return this.cars.filter(car=>car.toLowerCase().includes(this.car_make.toLowerCase()))
+            return this.cars.filter(car=>car.make.includes(this.car_make.toLowerCase()))
 
           }else if (car_model != ''){
-            return this.cars.filter(car=>car.toLowerCase().includes(this.car_model.toLowerCase()))
+            return this.cars.filter(car=>car.model.toLowerCase().includes(this.car_model.toLowerCase()))
           }else{
             return this.cars
           }
